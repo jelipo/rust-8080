9459e6741df2e1cfbf484d0c412bbb1368c2889d
jelipo <me@jelipo.com>
2020-07-23 17:10:21 +0800

    update

--- src/cpu/register.rs
+++ src/cpu/register.rs
@@ -12,11 +12,11 @@ pub struct Register {
 }
 
 impl Register {
-    pub fn bc(&self) -> u16 { (self.b as u16) << 8 | (self.c as u16) }
+    pub fn get_bc(&self) -> u16 { (self.b as u16) << 8 | (self.c as u16) }
 
-    pub fn de(&self) -> u16 { (self.d as u16) << 8 | (self.e as u16) }
+    pub fn get_de(&self) -> u16 { (self.d as u16) << 8 | (self.e as u16) }
 
-    pub fn hl(&self) -> u16 { (self.h as u16) << 8 | (self.l as u16) }
+    pub fn get_hl(&self) -> u16 { (self.h as u16) << 8 | (self.l as u16) }
 
 
     pub fn set_bc(&mut self, value: u16) {
@@ -25,7 +25,7 @@ impl Register {
     }
 
     pub fn set_de(&mut self, value: u16) {
-        self.d = (value >> 8) as u8;
+        self.d = (value >> 8) as u8;get_
         self.e = value as u8;
     }
 
